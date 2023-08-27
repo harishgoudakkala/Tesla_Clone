93c6c52866483e98b73ddc291434b1430401fb73
harishgoudakkala <harishgoudakkala@gmail.com>
2023-08-27 18:02:51 +0530

    first Commit

---
+++ src/Components/Section.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import styled from "styled-components"
+
+function Section() {
+  return (
+    <box>
+      Section
+    </box>
+  )
+}
+
+export default Section
+
+
+const box = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background: orange;
+`
